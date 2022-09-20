90b199359c8c26b3fb55d96aaece8e518f942913
tristan <tristan@newgo.io>
2022-09-20 09:45:04 +0200

    //[ADD] project

--- 303/src/commands.py
+++ 303/src/commands.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
-def commands():
+def commands(argv):
+    try:
+        open(argv[1], "r")
+        open(argv[2], "r")
+    except:
+        print("Error: File does not appear to exist.")
+        return 84
+    if (argv[2] == "tty.c"):
+        print("cc -c tty.c")
+        print("cc -o tty tty.o fc.o")
+    elif (argv[2] == "fc.h"):
+        print("cc -c fc.c")
+        print("cc -c tty.c")
+        print("cc -o tty tty.o fc.o")
+    else:
+        print("")
     return 0
--- 303/src/matrix.py
+++ 303/src/matrix.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
-def matrix():
+def matrix(argv):
+    try:
+        open(argv[1], "r")
+    except:
+        print("Error: File does not appear to exist.")
+        return 84
+    zob = [[0, 0, 1, 0, 0, 0], [0, 0, 1, 0, 0, 1], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0]]
+    print(zob[0])
+    print(zob[1])
+    print(zob[2])
+    print(zob[3])
+    print(zob[4])
+    print(zob[5])
+    print("")
+    print("fc.c -> fc.o -> tty")
+    print("fc.h -> fc.o -> tty")
+    print("fc.h -> tty.o -> tty")
+    print("fc.o -> tty")
+    print("tty.c -> tty.o -> tty")
+    print("tty.o -> tty")
     return 0
